972806dbcd1ca0520b7f6a752c05b52b4ecd0a8d
Mitsu Matsuo <75003091+mitsumatsuo@users.noreply.github.com>
2022-07-19 21:37:28 +0900

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [state, setState] = useState({
+    A: false,
+    B: false,
+    C: false,
+    D: false,
+    E: false,
+  });
+
+  const handleChange = (e) => {
+    setState((s) => {
+      return {
+        ...s,
+        [e.target.id]: !s[e.target.id],
+      };
+    });
+  };
+
+  return (
+    <div className="">
+      <div className="relative container">
+        {state.A ? <div className="ItemA Box">選択肢A</div> : null}
+        {state.B ? <div className="ItemB Box">選択肢B</div> : null}
+        {state.C ? <div className="ItemC Box">選択肢C</div> : null}
+        {state.D ? <div className="ItemD Box">選択肢D</div> : null}
+        {state.E ? <div className="ItemE Box">選択肢E</div> : null}
+      </div>
+      <div className="relative checkbox-container">
+        <div className="Check">
+          <input type="checkbox" name="A" id="A" onChange={handleChange} />
+          <label htmlFor="A">選択肢A</label>
+        </div>
+        <div className="Check">
+          <input type="checkbox" name="B" id="B" onChange={handleChange} />
+          <label htmlFor="B">選択肢B</label>
+        </div>
+        <div className="Check">
+          <input type="checkbox" name="C" id="C" onChange={handleChange} />
+          <label htmlFor="C">選択肢C</label>
+        </div>
+        <div className="Check">
+          <input type="checkbox" name="D" id="D" onChange={handleChange} />
+          <label htmlFor="D">選択肢D</label>
+        </div>
+        <div className="Check">
+          <input type="checkbox" name="E" id="E" onChange={handleChange} />
+          <label htmlFor="E">選択肢E</label>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
